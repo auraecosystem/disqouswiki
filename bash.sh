54085cfa0c267100fb955e6597fa5216e401734e
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-14 19:21:54 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,18 @@ $ cd ~/public_html
 $ php -S localhost:8000
 $ php -S 0.0.0.0:8000
 $ php -S localhost:8000 router.php
+
+analyzer --cfg CharaActor
+analyzer --callgraph Renderer
+analyzer --generate cpp
+analyzer --generate rust
+analyzer --generate zig
+analyzer --uml
+analyzer --graph
+analyzer --json
+analyzer --diff old.exe new.exe
+analyzer --plugins
+analyzer --ai
+analyzer --report html
+analyzer --report pdf
+analyzer --sdk
