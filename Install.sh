ee3d53e2705ab45a6e00d1829952dad5881a092b
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-14 20:03:20 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,1 +1,15 @@
 choco install -y php composer
+git clone https://gerrit.wikimedia.org/r/mediawiki/core.git mediawiki 
+git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector.git mediawiki/skins/Vector
+cd mediawiki
+composer mw-install:sqlite
+sudo apt install php php-intl php-mbstring php-xml php-apcu php-curl php-sqlite3 composer
+sudo pacman -S php composer
+sudo dnf install php composer
+git clone https://gerrit.wikimedia.org/r/mediawiki/core.git mediawiki 
+git clone https://gerrit.wikimedia.org/r/mediawiki/skins/Vector.git mediawiki/skins/Vector
+cd mediawiki
+composer update
+composer mw-install:sqlite
+brew install php@8.5 composer
+brew link --overwrite php@8.5
